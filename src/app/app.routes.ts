778d6580e562414e8ea16c391b4fd69f0bba3459
Patrick <echomabon@gmail.com>
2023-12-03 11:34:36 +0100

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'blog/:slug',
+    component: BlogPostComponent,
+  },
+];
---
+++ src/app/blog-post/blog-post.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-blog-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-post.component.html',
+  styleUrl: './blog-post.component.scss',
+})
+export class BlogPostComponent {
+  slug: string | undefined;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.slug = params['slug'];
+    });
+  }
+}
